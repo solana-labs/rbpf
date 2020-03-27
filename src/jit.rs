fe5eeef1d1db3007369131a7574bed1e1403dce2
Jack May <jack@solana.com>
2020-03-27 16:42:34 -0700

    nudge

--- src/jit.rs
+++ src/jit.rs
@@ -446,7 +446,7 @@ struct JitMemory<'a> {
 }
 
 impl<'a> JitMemory<'a> {
-    fn new(num_pages: usize) -> JitMemory<'a> {
+    fn new(_num_pages: usize) -> JitMemory<'a> {
         #[cfg(windows)]
             {
                 panic!("JIT not supported on windows");
@@ -454,12 +454,12 @@ impl<'a> JitMemory<'a> {
         let contents: &mut[u8];
         #[cfg(not(windows))] // Without this block windows will fail ungracefully, hence the panic above
         unsafe {
-            let size = num_pages * PAGE_SIZE;
+            let size = _num_pages * PAGE_SIZE;
             let mut raw: *mut libc::c_void = std::mem::MaybeUninit::uninit().assume_init();
             libc::posix_memalign(&mut raw, PAGE_SIZE, size);
             libc::mprotect(raw, size, libc::PROT_EXEC | libc::PROT_READ | libc::PROT_WRITE);
             std::ptr::write_bytes(raw, 0xc3, size);  // for now, prepopulate with 'RET' calls
-            contents = std::slice::from_raw_parts_mut(raw as *mut u8, num_pages * PAGE_SIZE);
+            contents = std::slice::from_raw_parts_mut(raw as *mut u8, _num_pages * PAGE_SIZE);
         }
 
         JitMemory {
