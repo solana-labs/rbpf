a04c39ca00db46dcdc265935d164727651813a32
Alexander Meißner <AlexanderMeissner@gmx.net>
2022-05-09 15:07:56 +0200

    Adjusts disclaimers.

--- src/interpreter.rs
+++ src/interpreter.rs
@@ -1,4 +1,15 @@
 #![allow(clippy::integer_arithmetic)]
+// Derived from uBPF <https://github.com/iovisor/ubpf>
+// Copyright 2015 Big Switch Networks, Inc
+//      (uBPF: VM architecture, parts of the interpreter, originally in C)
+// Copyright 2016 6WIND S.A. <quentin.monnet@6wind.com>
+//      (Translation to Rust, MetaBuff/multiple classes addition, hashmaps for syscalls)
+// Copyright 2020 Solana Maintainers <maintainers@solana.com>
+//
+// Licensed under the Apache License, Version 2.0 <http://www.apache.org/licenses/LICENSE-2.0> or
+// the MIT license <http://opensource.org/licenses/MIT>, at your option. This file may not be
+// copied, modified, or distributed except according to those terms.
+
 //! Interpreter for eBPF programs.
 
 use crate::{
